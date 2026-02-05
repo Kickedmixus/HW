2c37c230aeacd15f4e332e044bd9fd122f99fae4
mixus <134115767+Kickedmixus@users.noreply.github.com>
2026-02-04 22:10:47 -0800

    Update coin.py
    Finnaly fixed bug, and added remainder case

--- coin.py
+++ coin.py
@@ -4,10 +4,12 @@ def get_coins(amount,denomination_id=0,coins_collected={"Quarter":0,"Dime":0,"Ni
   denominations = {25:"Quarter",10:"Dime",5:"Nickel",1:"Penny"}
   
   if amount == 0 or denomination_id >= len(denominations):
+    if amount != 0:
+      coins_collected.update({"Remainder":amount})
     return coins_collected
   else:
-    fit = amount // denominations.keys()[denomination_id]
-    coins_collected[denominations[denominations.keys()[denomination_id]]] += int(fit)
-    return get_coins(amount-(fit*denominations.keys()[denomination_id]),denomination_id+1,coins_collected)
+    fit = amount // denominations.keys()[::-1][denomination_id]
+    coins_collected[denominations[denominations.keys()[::-1][denomination_id]]] += int(fit)
+    return get_coins((amount-(fit*denominations.keys()[::-1][denomination_id]))/100,denomination_id+1,coins_collected)
     
 print get_coins(5.76)
